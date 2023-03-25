a82f72a477aa4ed865b480035a72a0139f7913c4
Joshua Owusu <joshbillion1@gmail.com>
2023-03-25 08:31:29 +0000

    Commit auto_update

--- Todo/src/components/Header/index.jsx
+++ Todo/src/components/Header/index.jsx
@@ -4,7 +4,7 @@ import styles from './header.module.css';
 export function Header(){
     return (
         <header className={styles.header}>
-            <p>eader</p>
+            <p>newHeader</p>
         </header>
     )
 }
